978559ad3fc069f1ce97b3dbb5e1fa01836c5faf
Brandon Stewart <boveus@gmail.com>
2017-07-12 22:04:34 -0600

    Add refactor of insert method / submethods.

--- lib/tree.rb
+++ lib/tree.rb
@@ -13,22 +13,24 @@ class Tree
     @number_of_words
   end
 
-  def populate_children(node, converted_word, index, final_index)
-    letter = converted_word[index]
-    check_children(node, letter, converted_word, index, final_index)
+  def insert(word)
+    converted_word = convert_word_to_array(word)
+    check_children(converted_word)
   end
 
-  def check_children(node, letter, converted_word, index, final_index)
+  def check_children(converted_word, node = @root, index = 0, final_index = 0)
+    letter = converted_word[index]
+    final_index = converted_word.length
     if node.has_no_child?(letter) && index < final_index
       node.add_child(Node.new, letter)
       index += 1
-      populate_children(node.get_child(letter), converted_word, index, final_index)
+      check_children(converted_word, node.get_child(letter), index, final_index)
     elsif node.has_no_child?(letter) && index == final_index
         node.set_word
       return @number_of_words += 1
     elsif node.has_child?(letter) && index < final_index
       index += 1
-      populate_children(node.get_child(letter), converted_word, index, final_index)
+      check_children(converted_word, node.get_child(letter), index, final_index)
     elsif node.has_child?(letter) && index == final_index
       node.set_word
       return @number_of_words += 1
@@ -39,12 +41,7 @@ class Tree
     word.chars
   end
 
-  def insert(word)
-    index = 0
-    converted_word = convert_word_to_array(word)
-    final_index = converted_word.length
-    populate_children(@root, converted_word, index, final_index)
-  end
+
 
   def suggest(word_fragment)
     word_fragment_array = convert_word_to_array(word_fragment)
